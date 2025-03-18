b1d3127eea4b79addc87ed1a2a76e5bdf1ab04b4
haaben-exe <htkidanu@gmail.com>
2025-03-18 15:51:28 -0400

    updated userSchema

--- models/user.js
+++ models/user.js
@@ -4,11 +4,24 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
+    minLength: [5, 'Username cannot be less than 5 characters'],
+    maxLength: [25, 'Username cannot be more than 25 characters'],
+    trim: true
   },
   hashedPassword: {
     type: String,
     required: true,
   },
+  bio: {
+    type: String,
+    maxLength: [500, 'Bio cannot exceed 500 characters']
+  },
+  profileImage: {
+    type: String,
+    default: 'default-profile.jpg'
+  },
+
 });
 
 userSchema.set('toJSON', {
